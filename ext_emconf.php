40e8b43929f70bd0c030e3aa0367147594fbc087
Oliver Bartsch <bo@cedev.de>
2021-12-21 18:05:41 +0100

    [TASK] Add publish workflow

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,8 +3,20 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'b13 make',
     'description' => 'Kickstart functionality for TYPO3',
-    'category' => 'be',
+    'category' => 'misc',
+    'author' => 'b13 GmbH',
+    'author_email' => 'typo3@b13.com',
+    'author_company' => 'b13 GmbH',
+    'state' => 'beta',
+    'clearCacheOnLoad' => true,
     'version' => '0.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-12.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
     'autoload' => [
         'psr-4' => [
             'B13\\Make\\' => 'Classes/',

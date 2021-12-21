a104d03d5d75facb6edd05c7fe74f2627f3f3382
Benni Mack <benjamin.mack@b13.com>
2021-12-21 17:27:59 +0100

    [BUGFIX] Add version to ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,6 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'b13 make',
     'description' => 'Kickstart functionality for TYPO3',
     'category' => 'be',
+    'version' => '0.1.0',
     'autoload' => [
         'psr-4' => [
             'B13\\Make\\' => 'Classes/',

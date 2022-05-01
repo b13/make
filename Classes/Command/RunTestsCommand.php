f5e95b9708e97dfc76b7dbaea7a4a13916611fca
ochorocho <jochen.roth@b13.com>
2022-05-01 19:24:23 +0200

    [FEATURE] Add command 'make:runtests'.
    Downloads runTests.sh and docker-compose.yml, modify for the selected extensions and put it into Build folder.

---
+++ Classes/Command/RunTestsCommand.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of TYPO3 CMS-based extension "b13/make" by b13.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ */
+
+namespace B13\Make\Command;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Filesystem\Exception\IOException;
+use Symfony\Component\Filesystem\Filesystem;
+use TYPO3\CMS\Core\Http\Client\GuzzleClientFactory;
+use TYPO3\CMS\Core\Package\PackageInterface;
+use TYPO3\CMS\Core\Service\MarkerBasedTemplateService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Command for creating docker based test environment for a TYPO3 extension
+ */
+class RunTestsCommand extends AbstractCommand
+{
+    /**
+     * @var SymfonyStyle $io
+     */
+    protected $io;
+
+    /**
+     * @var Filesystem $filesystem
+     */
+    protected $filesystem;
+
+    /**
+     * @var PackageInterface $package
+     */
+    protected $package;
+
+    protected function configure(): void
+    {
+        $this->setDescription('Setup runTests.sh to run tests, linter, cgl in a Docker environment');
+        $this->filesystem = GeneralUtility::makeInstance(Filesystem::class);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->io = new SymfonyStyle($input, $output);
+        $this->package = $this->askForPackage($this->io);
+
+        $templatesToCreate = [
+            [
+                'source' => 'https://raw.githubusercontent.com/TYPO3/styleguide/main/Build/Scripts/runTests.sh',
+                'target' => $this->package->getPackagePath() . 'Build/Scripts/runTests.sh'
+            ],
+            [
+                'source' => 'https://raw.githubusercontent.com/TYPO3/styleguide/main/Build/testing-docker/docker-compose.yml',
+                'target' => $this->package->getPackagePath() . 'Build/testing-docker/docker-compose.yml'
+            ]
+        ];
+
+        foreach ($templatesToCreate as $template) {
+            $this->prepareTemplate(
+                $template['source'],
+                $template['target']
+            );
+        }
+
+        return 0;
+    }
+
+    protected function prepareTemplate(string $source, string $target): void
+    {
+        try {
+            $template = $this->getGuzzleClient()->get($source);
+        } catch (GuzzleException $exception) {
+            $this->io->writeln('<error>Failed to get remote file ' . $source . '</error>');
+            return;
+        }
+
+        $markerService = GeneralUtility::makeInstance(MarkerBasedTemplateService::class);
+        $templateContent = $markerService->substituteMarker(
+            $template->getBody(),
+            'styleguide',
+            $this->package->getPackageKey()
+        );
+
+        try {
+            $this->filesystem->dumpFile($target, $templateContent);
+        } catch (IOException $exception) {
+            $this->io->writeln('<error>Failed to save file in ' . $target . PHP_EOL . $exception->getMessage() . '</error>');
+        }
+    }
+
+    protected function getGuzzleClient(): Client
+    {
+        return GeneralUtility::makeInstance(GuzzleClientFactory::class)->getClient();
+    }
+}

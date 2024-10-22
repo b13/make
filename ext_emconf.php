befcd9694f94f152a9f4a182ef320e4e774469b4
Josef Glatz <josefglatz@gmail.com>
2024-10-22 16:32:03 +0200

    [TASK] Update TYPO3 core version constraint in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.1.8',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-13.2.99',
+            'typo3' => '10.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
